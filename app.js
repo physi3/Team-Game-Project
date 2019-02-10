dda475d2aabb3d21585d7e69d2030490f327eea7
ollydevulder <ollydevulder@gmail.com>
2019-02-10 22:30:59 +0000

    £

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
-var port = process.env.PORT || 8080; // either run globally or locally
+var port = process.env.PORT || 8080; // either run by given port (heroku) or locally on 8080
 app.use(express.static(__dirname + '/public')); // get assets
 
 app.get('/', (req, res) => {
-    res.render('home/index');
+    res.redirect('/home');
+});
+
+app.get('/home', (req, res) => {
+    res.render('home');
 });
 
 app.get('/game', (req, res) => {
-    res.render('game/index');
+    res.render('game');
 });
 
 app.listen(port, () => {
