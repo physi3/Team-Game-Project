4e78fb04126bb6b2063fd335884256f022b72727
joemarriage <45903919+joemarriage@users.noreply.github.com>
2019-02-12 21:07:25 +0000

    Large Update

--- public/js/game.js
+++ public/js/game.js
@@ -1,35 +1,96 @@
-let last = 0;
+let scale = 1;
+let last = 100;
+let square;
+let back = [];
+let darker = false;
+let colorU;
+var xOffset = 0.0; 
+var yOffset = 0.0;
+var locked = false;
+var bx = 100;
+var by = 100;
+
+class Square {
+  constructor(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.originalColor = color;
+  }
+  XAppearance() {
+    return ((this.x*scale)+bx);
+  }
+  YAppearance() {
+    return ((this.y*scale)+by);
+  }
+  sizeAppearance() {
+    return (100 * scale)
+  }  
+  show() {
+    fill(this.color)
+    rect(this.XAppearance(), this.YAppearance(),this.sizeAppearance(),this.sizeAppearance())
+  }
+  hovering() {
+    return mouseX > this.XAppearance() && mouseX < this.XAppearance() + this.sizeAppearance() && mouseY > this.YAppearance() && mouseY < this.YAppearance() + this.sizeAppearance();
+  }
+}
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
   noStroke();
+  for (let i = 0; i <= 60; i++) {
+    back[i] = []
+    for (let j = 0; j <= 40; j++) {
+      darker = !darker
+      if (darker == true) {
+        colorU = color(3,3,3)
+      } else {
+        colorU = color(15,15,15)
+      }
+      back[i][j] = new Square(i*100, j*100, colorU);
+    }
+  }
 }
 
 function draw() {
-  background(3);
-  back(70,40,last/1000);
-}
-
-function back(x,y,scale) {
-	for (let i = 0; i <= 100; i++) {
-  	for (let j = 0; j <= 200; j++) {
-    	fill(6)
-      x1 = x-4000+i*56-1+(27*j)
-      y1 = y-4000+55+j*55
-      x2 = x-4000+28+i*56+(27*j)
-      y2 = y-4000+j*55
-      x3 = x-4000+56+i*56+(27*j)
-      y3 = y-4000+55+j*55
-      triangle(x1*scale,y1*scale,x2*scale,y2*scale,x3*scale,y3*scale);
-      fill(256)
+  scale = last/1000;
+  background(100)
+  for (let i = 0; i < back.length; i++) {
+    for (let j = 0; j < back[i].length; j++) {	
+      back[i][j].color = back[i][j].originalColor;
+      if (back[i][j].hovering() == true) {
+        back[i][j].color = 150;
+      }
+    }
+  }
+  back[10][10].color = color(250,250,0)
+  for (let i = 0; i < back.length; i++) {
+    for (let j = 0; j < back[i].length; j++) {
+      back[i][j].show();
     }
   }
 }
 
 function mouseWheel(event) {
-  print(event.delta*-1+last);
   last = event.delta*-1+last
   if (last <= 0){
   	last = 100
   }
+}
+
+function mousePressed() { 
+  locked = true; 
+  xOffset = mouseX-bx; 
+  yOffset = mouseY-by; 
+
+}
+function mouseDragged() {
+  if(locked) {
+    bx = mouseX-xOffset; 
+    by = mouseY-yOffset; 
+  }
+}
+
+function mouseReleased() {
+  locked = false;
 }
