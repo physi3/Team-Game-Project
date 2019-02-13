16c14af4b56edda95d19b997ac3ea2130cfcb492
joemarriage <45903919+joemarriage@users.noreply.github.com>
2019-02-13 18:16:58 +0000

    Added Square Images

--- public/js/game.js
+++ public/js/game.js
@@ -1,3 +1,4 @@
+
 let scale = 1;
 let last = 100;
 let square;
@@ -7,8 +8,24 @@ let colorU;
 var xOffset = 0.0; 
 var yOffset = 0.0;
 var locked = false;
-var bx = 100;
-var by = 100;
+var bx = 0;
+var by = 0;
+ //Various variable declorations
+
+var sunImg=[];
+
+//Specific Images
+function addSun(centerX,centerY) {
+  back[centerX-1][centerY-1].addImage(sunImg[1])
+  back[centerX][centerY-1].addImage(sunImg[2])
+  back[centerX+1][centerY-1].addImage(sunImg[3])
+  back[centerX-1][centerY].addImage(sunImg[4])
+  back[centerX][centerY].addImage(sunImg[5])
+  back[centerX+1][centerY].addImage(sunImg[6])
+  back[centerX-1][centerY+1].addImage(sunImg[7])
+  back[centerX][centerY+1].addImage(sunImg[8])
+  back[centerX+1][centerY+1].addImage(sunImg[9])
+}
 
 class Square {
   constructor(x, y, color) {
@@ -16,7 +33,10 @@ class Square {
     this.y = y;
     this.color = color;
     this.originalColor = color;
+    this.img = "None";
+
   }
+  
   XAppearance() {
     return ((this.x*scale)+bx);
   }
@@ -25,19 +45,37 @@ class Square {
   }
   sizeAppearance() {
     return (100 * scale)
-  }  
+  }
+
   show() {
     fill(this.color)
     rect(this.XAppearance(), this.YAppearance(),this.sizeAppearance(),this.sizeAppearance())
+    if (this.img != "None"){
+      print(this.img, this.XAppearance(), this.YAppearance(),this.sizeAppearance(), this.sizeAppearance())
+      image(this.img, this.XAppearance(), this.YAppearance(),this.sizeAppearance(), this.sizeAppearance())
+    }
   }
   hovering() {
     return mouseX > this.XAppearance() && mouseX < this.XAppearance() + this.sizeAppearance() && mouseY > this.YAppearance() && mouseY < this.YAppearance() + this.sizeAppearance();
   }
-}
+
+  addImage(img) {
+    this.img = img
+  }
+  removeImage() {
+    this.img = "None"
+  }
+
+} //Sqaure Class
 
 function setup() {
+  for (let i = 0; i <= 9; i++) {
+    sunImg[i] = loadImage("../icons/sun/"+i+".png")
+  }
+
   createCanvas(600, 400);
   noStroke();
+
   for (let i = 0; i <= 60; i++) {
     back[i] = []
     for (let j = 0; j <= 40; j++) {
@@ -49,27 +87,32 @@ function setup() {
       }
       back[i][j] = new Square(i*100, j*100, colorU);
     }
-  }
+  } //Calculate Background Sqaures
 }
 
 function draw() {
+  print(sunImg)
   scale = last/1000;
   background(100)
+  
   for (let i = 0; i < back.length; i++) {
     for (let j = 0; j < back[i].length; j++) {	
       back[i][j].color = back[i][j].originalColor;
       if (back[i][j].hovering() == true) {
-        back[i][j].color = 150;
+        back[i][j].color = back[i][j].originalColor+10;
       }
     }
-  }
-  back[10][10].color = color(250,250,0)
+  } //Highlight Hovering Sqaures
+
+  addSun(10,10);
+
   for (let i = 0; i < back.length; i++) {
     for (let j = 0; j < back[i].length; j++) {
       back[i][j].show();
     }
   }
 }
+
 
 function mouseWheel(event) {
   last = event.delta*-1+last
