c9b9bb3e75dab9872801dab175c1ec0714d72239
ollydevulder <ollydevulder@gmail.com>
2019-02-16 15:45:42 +0000

    fixed force_https

--- middleware/force_https.js
+++ middleware/force_https.js
@@ -1,11 +1,8 @@
 module.exports = { forceHttps };
 
 function forceHttps(req, res, next) {
-/*     if (!req.secure && req.hostname != 'localhost') {
+     if (!req.secure && req.hostname != 'localhost') {
         res.redirect('https://' + req.hostname + req.originalUrl);
-    } */
-    console.log(req.secure);
-    console.log(req.protocol);
-    console.log(req.connection.encrypted);
+    } 
     next();
 }
