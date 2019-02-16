77c9c6e3d5c8073a9914e44046b90a06b404b2f3
ollydevulder <ollydevulder@gmail.com>
2019-02-16 15:52:45 +0000

    working now

--- middleware/force_https.js
+++ middleware/force_https.js
@@ -3,7 +3,6 @@ module.exports = { forceHttps };
 function forceHttps(req, res, next) {
      if (!req.secure && req.hostname != 'localhost') {
         res.redirect('https://' + req.hostname + req.originalUrl);
-          console.log('Redirected to https!');
-    } 
-    next();
+    } else {
+        next();
 }
